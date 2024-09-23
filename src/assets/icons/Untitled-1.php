724d69e20b5e867c3f79c4a2e9624548aa887531
Ayan <md.ayan835@gmail.com>
2024-09-23 23:56:30 +0530

    first commit

---
+++ src/assets/icons/Untitled-1.php
@@ -0,0 +1,9 @@
+<path fill="rgb(238,249,241)" fill-opacity="1" d=" M-261,-317.5 C-261,-328.5450134277344 -252.04600524902344,-337.5 -241,-337.5 C-241,-337.5 241,-337.5 241,-337.5 C252.04600524902344,-337.5 261,-328.5459899902344 261,-317.5 C261,-317.5 261,317.5 261,317.5 C261,328.5459899902344 252.04600524902344,337.5 241,337.5 C241,337.5 -241,337.5 -241,337.5 C-252.04600524902344,337.5 -261,328.5459899902344 -261,317.5 C-261,317.5 -261,-317.5 -261,-317.5z"></path>
+
+
+
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 522 675" width="522" height="675" preserveAspectRatio="xMidYMid meet" style="width: 100%; height: 100%; transform: translate3d(0px, 0px, 0px); content-visibility: visible;"></svg>
+
+{/* <!-- Slider Controls --> */} 
+{/* <button class="prev" onclick="moveSlide(-1)">&#10094;</button>
+<button class="next" onclick="moveSlide(1)">&#10095;</button>  */}
